6e3ad2d856dd97f52dcef7cead4c8c657561180f
Uice Lu <uicestone@gmail.com>
2014-06-24 09:36:14 +0800

    微信API响应页面

---
+++ wp-content/themes/cmcc-skin/page-wx.php
@@ -0,0 +1,22 @@
+<?php
+/** 
+ * 微信API响应页面，用来处理来自微信的请求
+ */
+$wx = new WeixinAPI();
+		
+// 验证请求来自微信
+$wx->verify();
+
+if(isset($_GET['echostr'])){
+	echo $_GET['echostr'];
+}
+
+if(isset($GLOBALS["HTTP_RAW_POST_DATA"])){
+	xml_parse_into_struct(xml_parser_create(), $GLOBALS["HTTP_RAW_POST_DATA"], $post);
+
+	$post=array_column($post,'value','tag');
+
+	if(!is_array($post)){
+		exit('XML parse error.');
+	}
+}
