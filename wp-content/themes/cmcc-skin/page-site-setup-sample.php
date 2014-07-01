b61100f69feea7601238aaa32f6c23256ec24872
Uice Lu <uicestone@gmail.com>
2014-07-01 15:04:12 +0800

    多种安装示范的展示

--- wp-content/themes/cmcc-skin/page-site-setup-sample.php
+++ wp-content/themes/cmcc-skin/page-site-setup-sample.php
@@ -1,43 +1,21 @@
 <?php get_header(); ?>
 
 <header>
-	<h1>2014 移动营业厅物料安装规范</h1>
-	<h2>悬挂灯箱（小）安装示意图</h2>
+	<h1>物料 / 画面安装规范</h1>
 </header>
 
-<div class="row sample">
-	<div class="col-xs-6 correct-sample">
-		<h3>正确安装示意图：</h3>
-		<div class="step">
-			<img src="<?=get_template_directory_uri()?>/img/huanzhuan-1.png" />
-			<p>1. 打开灯箱四周开启式弹簧压条</p>
-		</div>
-		<div class="step">
-			<img src="<?=get_template_directory_uri()?>/img/huanzhuan-2.png" />
-			<p>2. 打开灯箱四周开启式弹簧压条</p>
-		</div>
-		<div class="step">
-			<img src="<?=get_template_directory_uri()?>/img/huanzhuan-3.png" />
-			<p>3. 打开灯箱四周开启式弹簧压条</p>
-		</div>
-		<span class="mark"></span>
-	</div>
-	<div class="col-xs-6 wrong-sample">
-		<h3>错误安装示意图：</h3>
-		<div class="step">
-			<img src="<?=get_template_directory_uri()?>/img/huanzhuan-4.png" />
-			<p>1. 打开灯箱四周开启式弹簧压条</p>
-		</div>
-		<div class="step">
-			<img src="<?=get_template_directory_uri()?>/img/huanzhuan-5.png" />
-			<p>2. 打开灯箱四周开启式弹簧压条</p>
-		</div>
-		<div class="step">
-			<img src="<?=get_template_directory_uri()?>/img/huanzhuan-6.png" />
-			<p>3. 打开灯箱四周开启式弹簧压条</p>
-		</div>
-		<span class="mark"></span>
-	</div>
-</div>
-
+<?php if(empty($_GET['ID'])){ $examples = get_posts(array('posts_per_page'=>-1)); ?>
+<table class="table table-bordered detail summary">
+	<tbody>
+		<?php foreach($examples as $example){ ?>
+		<tr>
+			<td><a href="<?=site_url()?>/site-setup-sample/?ID=<?=$example->ID?>"><?=$example->post_title?></a></td>
+		</tr>
+		<?php } ?>
+	</tbody>
+</table>
+<?php } else{ $example = get_post($_GET['ID']); ?>
+<h2><?=$example->post_title?></h2>
+<?=$example->post_content?>
+<?php } ?>
 <?php get_footer(); ?>
